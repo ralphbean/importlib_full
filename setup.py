0cb5f68b173f991283fb679e4493ec7af08b1fab
Ralph Bean <rbean@redhat.com>
2015-11-30 13:05:27 -0500

    0.0.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ finally:
 
 setup(
     name='importlib_full',
-    version='0.0.1',
+    version='0.0.2',
     description="Abandoned.  Do not use.",
     long_description=detailed_description,
     author='Ralph Bean',
